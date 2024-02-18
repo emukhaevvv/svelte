6ebb521223953a6f0584211bae445ebf4c5c5688
emukhaevvv <emukhaevvv@yandex.ru>
2024-02-18 10:36:46 +0300

    The Single File Component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,16 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang='ts'>
+    let count = 0;
+
+    function updateCount() {
+        count += 1;
+    }
+</script>
+
+<p>Clicked {count} {count === 1 ? "time" : "times"}</p>
+<button on:click={updateCount}>Increment</button>
+
+<style>
+    p {
+        color: teal;
+    }
+</style>
