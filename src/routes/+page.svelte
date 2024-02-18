ebbb99b673ba441630be52379d0bc4f229254840
emukhaevvv <emukhaevvv@yandex.ru>
2024-02-18 11:22:40 +0300

    Reactivity

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,38 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    let list = [{id: 1, title: "Title for Item"}];
+
+    const addNewItem = () => {
+        list = [...list, {id: list.length + 1, title: "Title for Item"}]
+    }
+
+    $: amount = list.length;
+</script>
+
+<div class="content">
+    <ul>
+        {#each list as item}
+            <li>
+                <div>id: {item.id}</div>
+                <p>{item.title}</p>
+            </li>
+        {/each}
+    </ul>
+
+    <div class="right">
+        <button on:click={addNewItem}>Add new Item</button>
+        <div>Total items amount: {amount}</div>
+    </div>
+</div>
+
+<style>
+    .content {
+        display: flex;
+        align-items: start;
+        gap: 40px;
+    }
+    li {
+        display: flex;
+        align-items: center;
+        gap: 20px;
+    }
+</style>
